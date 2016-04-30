358ddc8d9c4d93aa7cc60e44a63c8ff42708b53d
Nmae <newmae123456@gmail.com>
2016-04-30 18:58:41 +0700

    #03 สร้างดาวตกแต่โดนยานไม่ตาย

--- GameEngine.java
+++ GameEngine.java
@@ -13,11 +13,13 @@ import javax.swing.Timer;
 
 public class GameEngine implements GameReporter{
 	GamePanel gp;
-			
+	
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
 	private SpaceShip v;	
 	
 	private Timer timer;
 	
+
 	private double difficulty = 0.1;
 	
 	public GameEngine(GamePanel gp, SpaceShip v) {
@@ -40,16 +42,36 @@ public class GameEngine implements GameReporter{
 	public void start(){
 		timer.start();
 	}
+
+	private void generateEnemy(){
+		Enemy e = new Enemy((int)(Math.random()*390), 30);
+		gp.sprites.add(e);
+		enemies.add(e);
+	}
 	
 	private void process(){
+		if(Math.random() < difficulty){
+			generateEnemy();
+		}
+		
+		Iterator<Enemy> e_iter = enemies.iterator();
+		while(e_iter.hasNext()){
+			Enemy e = e_iter.next();
+			e.proceed();
+			
+			if(!e.isAlive()){
+				e_iter.remove();
+				gp.sprites.remove(e);
+			}
+		}
 		
 		gp.updateGameUI(this);
 		
 		Rectangle2D.Double vr = v.getRectangle();
 		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+		}
 	}
-	
-	public void die(){
-		timer.stop();
-	}
+
 }
